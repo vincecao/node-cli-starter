d3ded68059b4cf1d14b4e63a521fd3462757ec72
Lineng Cao <linengca@usc.edu>
2021-02-06 22:38:07 -0800

    update jest test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from 'App';
+import Button from 'components/Button';
 
-test('renders learn react link', () => {
+jest.mock('components/Button', () => ({
+  __esModule: true,
+  default: () => <>mock-test</>,
+}));
+
+test('renders App test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/and save to reload/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders jest mock react component with root import test', () => {
+  render(<Button />);
+  const linkElement = screen.queryByText('mock-test');
   expect(linkElement).toBeInTheDocument();
 });
