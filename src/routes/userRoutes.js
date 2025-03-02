abaefaac1df556f11967816d410896cb6b07c73c
Lineng Cao <linengca@usc.edu>
2025-03-02 02:30:06 -0800

    init: js starter kit

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
+
+const router = express.Router();
+
+// User routes
+router.get('/', getUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+
+export default router;
